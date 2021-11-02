efcc2afbe1897d0d355164ca536440a16089b6eb
Anger romain <anger.romain@gmail.com>
2021-11-02 21:29:07 +0100

    fonction theme et style de jeu terminer, mise en place des 12 cartes

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ const els = {
 }
 
 // Déclaration des variables
-let theme = "";
+let gameSelect = [];
+
 /*#####################################################################
 ##                        Initialisation du jeu                      ##
 #####################################################################*/
@@ -22,28 +23,42 @@ const init = () => {
 
 
   displayThemeChoices();
-  console.log(theme);
+  console.log(gameSelect);
 
   // écoute du click sur le choix des themes
   els.dispThemes.addEventListener('click', ({
     target
   }) => {
     if (target.matches('#themeEnfant')) {
-      theme = "enfant";
-      //console.log(theme);
-      displayDifficultChoices(theme);
+      gameSelect.push("enfant");
+      console.log(gameSelect);
+      displayDifficultChoices(gameSelect);
     } else if (target.matches('#themeLangage')) {
-      theme = "langage";
-      //console.log(theme);
-      displayDifficultChoices(theme);
+      gameSelect.push("langage");
+      console.log(gameSelect);
+      displayDifficultChoices(gameSelect);
     } else if (target.matches('#themeSerie')) {
-      theme = "serie";
-      //console.log(theme);
-      displayDifficultChoices(theme);
+      gameSelect.push("serie");
+      console.log(gameSelect);
+      displayDifficultChoices(gameSelect);
     }
   });
 
-  
+  // écoute du click sur le choix des difficultées
+  els.dispDifficult.addEventListener('click', ({
+    target
+  }) => {
+    if (target.matches('#btnTimeVs')) {
+      gameSelect.push("timeVs");
+      console.log(gameSelect);
+      displayCardsGenerate(gameSelect);
+    } else if (target.matches('#btnPlayMoves')) {
+      gameSelect.push("playMoves");
+      console.log(gameSelect);
+      displayCardsGenerate(gameSelect);
+    }
+  });
+
 };
 
 /*#####################################################################
@@ -60,19 +75,41 @@ const displayThemeChoices = () => {
         <button id="themeLangage" class="d-flex col-8 justify-content-center align-items-center py-3 my-3">Logos Langages</button>
         <button id="themeSerie" class="d-flex col-8 justify-content-center align-items-center py-3 my-3">Séries</button>
       </div>
-    `
+    `;
 };
 
 // Fonction d'affichage du choix difficulté
 const displayDifficultChoices = () => {
+  // Effacement du menu des themes
   els.dispThemes.classList.replace("d-flex", "hidden");
-  console.log("dispdif : " ,theme);
+  // Affichage du menu des difficultées
   els.dispDifficult.innerHTML =
     `
-    
-    `
+    <h2 class="d-flex col-12 justify-content-center align-items-center my-5 mx-auto">Choisissez votre style de jeu :</h2>
+      <div id="difficulty" class="d-flex flex-column col-12 justify-content-center align-items-center">
+        <button id="btnTimeVs" class="d-flex col-8 justify-content-center align-items-center py-3 my-3">Contre la montre</button>
+        <button id="btnPlayMoves" class="d-flex col-8 justify-content-center align-items-center py-3 my-3">Nombre de coup</button>
+      </div>
+    `;
 };
 
+// Fonction d'affichage du plateau de carte
+const displayCardsGenerate = () => {
+  // Effacement du menu des difficultées
+  els.dispDifficult.classList.replace("d-flex", "hidden");
+  generateCards(gameSelect);
+};
+
+// Génération des cartes suivant le theme choisi
+const generateCards = (gameSelect) => {
+  console.log("generate fonction : ",gameSelect[0]);
+  if (gameSelect[0] == "enfant"){
+    els.allCards.innerHTML = 
+    `
+    
+    `;
+  }
+};
 
 window.addEventListener('load', () => {
   init();
